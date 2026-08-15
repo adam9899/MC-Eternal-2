0c1868f396f2aec931c58936d642691f6f73e884
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2026-08-15 16:16:25 -0700

    Add creative/spectator bypass to Dwarven Depths nightvision removal - for testing/debugging purposes

--- overrides/kubejs/server_scripts/dwarven_depths_darkness.js
+++ overrides/kubejs/server_scripts/dwarven_depths_darkness.js
@@ -1,26 +1,31 @@
 PlayerEvents.tick(event => {
-    if (event.player.level.dimension != "mce:dwarven_depths") return
- if ((event.player.persistentData.nvWarnCooldown || 0) > 0)
-        event.player.persistentData.nvWarnCooldown--
+    let {player} = event
+    
+    if (player.level.dimension != "mce:dwarven_depths" 
+        || (player.isCreative() || player.isSpectator()))
+            return;
 
-    if (event.player.hasEffect("minecraft:night_vision")) {
-        if ((event.player.persistentData.nvWarnCooldown || 0) <= 0) {
-            event.player.tell("§3The ancient dwarven magic has shrouded your sight... Nightvision will not work here.")
-            event.player.persistentData.nvWarnCooldown = 500 
+    if ((player.persistentData.nvWarnCooldown || 0) > 0)
+        player.persistentData.nvWarnCooldown--
+
+    if (player.hasEffect("minecraft:night_vision")) {
+        if ((player.persistentData.nvWarnCooldown || 0) <= 0) {
+            player.tell("§3The ancient dwarven magic has shrouded your sight... Nightvision will not work here.")
+            player.persistentData.nvWarnCooldown = 500 
         }
 
-        event.player.removeEffect("minecraft:night_vision")
+        player.removeEffect("minecraft:night_vision")
     }
-            const hasLantern =
-        event.player.mainHandItem.id == "ars_additions:golden_lantern" ||
-        event.player.offHandItem.id == "ars_additions:golden_lantern"
+    const hasLantern =
+        player.mainHandItem.id == "ars_additions:golden_lantern" ||
+        player.offHandItem.id == "ars_additions:golden_lantern"
 
     if (!hasLantern) {
-        if (!event.player.persistentData.darkwarned) {
-            event.player.tell("§7It is incomprehensibly dark.. You can just about see strange shadowy figures in the distance.. §oPerhaps I should equip a §6Golden Lantern")
-            event.player.persistentData.darkwarned = true
+        if (!player.persistentData.darkwarned) {
+            player.tell("§7It is incomprehensibly dark.. You can just about see strange shadowy figures in the distance.. §oPerhaps I should equip a §6Golden Lantern")
+            player.persistentData.darkwarned = true
         }
     } else {
-        event.player.persistentData.darkwarned = false
+        player.persistentData.darkwarned = false
     }
 })
